71a1a16ed0d49e7acb929ebdb8e023b81f19dcc9
albert-881 <quinteroalberto88@gmail.com>
2025-04-12 16:36:08 -0500

    deleted files

--- admin/js/adminDashboard.js
+++
@@ -1,6 +0,0 @@
-if (sessionStorage.getItem('isAdmin') === 'true') {
-    console.log('approved');
-} else {
-    alert('You need to login first');
-    window.location.href = 'adminLogin.html';  // Redirect to login page
-}
