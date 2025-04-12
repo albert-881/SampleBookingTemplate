e05ede291a066133eb9717d538be11c6d5082faf
albert-881 <quinteroalberto88@gmail.com>
2025-04-12 17:28:38 -0500

    button issue size fixed

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-document.addEventListener()
+document.addEventListener("DOMContentLoaded", function () {
+    if (window.location.hash) {
+        // Clear the hash and scroll to top
+        history.replaceState(null, null, window.location.pathname);
+        window.scrollTo(0, 0);
+    }
+});
